32e52d26ba555486507bea157ccb02ccaf66d61d
= <=>
2024-06-21 21:10:30 -0300

    criação dos mocks authorizados e notoficador.

--- src/main/java/com/rcm/api/pagamentos/ApiPagamentosApplication.java
+++ src/main/java/com/rcm/api/pagamentos/ApiPagamentosApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiPagamentosApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiPagamentosApplication.class, args);
 	}
 
 }
+
+// Gerar Mock
+// https://designer.mocky.io
